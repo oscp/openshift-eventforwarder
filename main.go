aca59935d2cc284f7ac35e10f46ca84ae40771be
u220374 <reto.lehmann@sbb.ch>
2017-03-27 14:34:31 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"bufio"
+	"log"
+	"encoding/json"
+	"golang.org/x/build/kubernetes/api"
+	"fmt"
+)
+
+type Stream struct {
+	Type string `json:"type,omitempty"`
+	Event api.Event `json:"object"`
+}
+
+func main() {
+	apiAddr := os.Getenv("OSE_API_ADDR")
+	apiToken := os.Getenv("OSE_TOKEN")
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://" + apiAddr + "/api/v1/events?watch=true", nil)
+	if (err != nil) {
+		log.Fatal("Error while opening connection", err)
+	}
+	req.Header.Add("Authorization", "Bearer " + apiToken)
+	resp, err := client.Do(req)
+
+	if (err != nil) {
+		log.Fatal("Error while connecting to:", apiAddr, err)
+	}
+
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if (err != nil) {
+			log.Fatal("Error reading from response stream.", err)
+		}
+
+		event := Stream{}
+		decErr := json.Unmarshal(line, &event)
+		if (decErr != nil) {
+			log.Fatal("Error decoding json", err)
+		}
+
+		fmt.Printf("Project: %v, Time: %v | Name: %v | Kind: %v | Reason: %v | Message: %v",
+			event.Event.Namespace, event.Event.LastTimestamp, event.Event.Name,
+			event.Event.Kind, event.Event.Reason, event.Event.Message)
+	}
+}
