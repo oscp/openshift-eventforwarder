17713eb663cc4a249da7e41dd798592ee25554e4
u220374 <reto.lehmann@sbb.ch>
2017-08-21 10:57:53 +0200

    Endless running fix if master api disappears

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"os"
-	"net/http"
 	"bufio"
-	"log"
-	"encoding/json"
-	"golang.org/x/build/kubernetes/api"
-	"fmt"
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/build/kubernetes/api"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Stream struct {
-	Type string `json:"type,omitempty"`
+	Type  string    `json:"type,omitempty"`
 	Event api.Event `json:"object"`
 }
 
@@ -24,33 +25,42 @@ func main() {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
-	req, err := http.NewRequest("GET", apiAddr + "/api/v1/events?watch=true", nil)
-	if (err != nil) {
-		log.Fatal("Error while opening connection", err)
+	req, err := http.NewRequest("GET", apiAddr+"/api/v1/events?watch=true", nil)
+	if err != nil {
+		log.Fatal("## Error while opening connection to openshift api", err)
 	}
-	req.Header.Add("Authorization", "Bearer " + apiToken)
-	resp, err := client.Do(req)
+	req.Header.Add("Authorization", "Bearer "+apiToken)
 
-	if (err != nil) {
-		log.Fatal("Error while connecting to:", apiAddr, err)
-	}
-
-	reader := bufio.NewReader(resp.Body)
 	for {
-		line, err := reader.ReadBytes('\n')
-		if (err != nil) {
-			log.Fatal("Error reading from response stream.", err)
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Println("## Error while connecting to:", apiAddr, err)
+			time.Sleep(5 * time.Second)
+			break
 		}
 
-		event := Stream{}
-		decErr := json.Unmarshal(line, &event)
-		if (decErr != nil) {
-			log.Fatal("Error decoding json", err)
-		}
+		reader := bufio.NewReader(resp.Body)
 
-		fmt.Printf("%v | Project: %v | Name: %v | Kind: %v | Reason: %v | Message: %v\n",
-			event.Event.LastTimestamp,
-			event.Event.Namespace, event.Event.Name,
-			event.Event.Kind, event.Event.Reason, event.Event.Message)
+		for {
+			line, err := reader.ReadBytes('\n')
+			if err != nil {
+				log.Println("## Error reading from response stream.", err)
+				break
+			}
+
+			event := Stream{}
+			decErr := json.Unmarshal(line, &event)
+			if decErr != nil {
+				log.Println("## Error decoding json", err)
+				break
+			}
+
+			fmt.Printf("%v | Project: %v | Name: %v | Kind: %v | Reason: %v | Message: %v\n",
+				event.Event.LastTimestamp,
+				event.Event.Namespace, event.Event.Name,
+				event.Event.Kind, event.Event.Reason, event.Event.Message)
+		}
 	}
 }
+
